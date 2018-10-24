affe5bbe5e0a1fa0752198a22943868af1ffae8c
tiancao <tiancao@cs.unc.edu>
2018-10-24 11:33:24 -0700

    updated model file

--- model.py
+++ model.py
@@ -1,6 +1,16 @@
 import keras
-from keras_self_attention import Attention
+import keras.backend as K
+import tensorflow as tf
+from keras_self_attention import SeqSelfAttention
 from keras_contrib.layers import CRF
+
+
+def attention_loss(factor=1e-6):
+    def attention_regularizer(y, y_pred):
+        input_len = K.shape(y_pred)[-1]
+        return factor * K.square(K.batch_dot(y_pred, K.permute_dimensions(y_pred, (0, 2, 1)))
+                                 - tf.eye(input_len))
+    return attention_regularizer
 
 
 def build_model(token_num,
@@ -35,10 +45,10 @@ def build_model(token_num,
                                                               recurrent_dropout=0.4,
                                                               return_sequences=True),
                                             name='Bi-LSTM')(embd_layer)
-    attention_layer = Attention(attention_activation='sigmoid',
-                                attention_width=9,
-                                return_attention=return_attention,
-                                name='Attention')(lstm_layer)
+    attention_layer = SeqSelfAttention(attention_activation='sigmoid',
+                                       attention_width=9,
+                                       return_attention=return_attention,
+                                       name='Attention')(lstm_layer)
     if return_attention:
         attention_layer, attention = attention_layer
     crf = CRF(units=tag_num, sparse_target=True, name='CRF')
@@ -47,7 +57,7 @@ def build_model(token_num,
     loss = {'CRF': crf.loss_function}
     if return_attention:
         outputs.append(attention)
-        loss['Attention'] = Attention.loss(1e-4)
+        loss['Attention'] = attention_loss(1e-4) # Attention.loss(1e-4)
 
     model = keras.models.Model(inputs=input_layer, outputs=outputs)
     model.compile(
